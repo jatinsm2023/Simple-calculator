50cf165585e1ceb331ef52e25ca3b7c5f96f9e05
Jatin Mahawar <131634000+jatinsm2023@users.noreply.github.com>
2023-06-29 08:21:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function DisplayNumber(Number) {
-    const PreviousElement = document.querySelector('.js-Number-Display').innerText;
+    let PreviousElement = document.querySelector('.js-Number-Display').innerText;
+
+    if(PreviousElement.length <13)
+    {
+        document.querySelector('.js-Number-Display').style.fontSize = "2rem";
+    }
+    if(PreviousElement.length >= 13 && PreviousElement.length <17)
+    {
+        document.querySelector('.js-Number-Display').style.fontSize = "1.7rem";
+   }
+    if(PreviousElement.length >= 17 && PreviousElement.length<23)
+    { 
+        document.querySelector('.js-Number-Display').style.fontSize = "1.3rem";
+    }
+    if(PreviousElement.length >= 23)
+    { 
+        document.querySelector('.js-Number-Display').style.fontSize = "1rem";
+    }
     document.querySelector('.js-Number-Display')
         .innerText = PreviousElement + Number;
 }
@@ -13,6 +30,27 @@ function DisplayClear() {
 
 function DisplayCalculation() {
     let PreviousCal = document.querySelector('.js-Number-Display').innerText;
+
+    if(PreviousCal.length < 8){
+        document.querySelector('.js-Answer-Display').style.fontSize = "3rem";
+    }
+
+    if(PreviousCal.length >= 8 && PreviousCal.length < 13){
+        document.querySelector('.js-Answer-Display').style.fontSize = "2rem";
+    }
+
+    if(PreviousElement.length >= 13 && PreviousElement.length <17)
+    {
+        document.querySelector('.js-Number-Display').style.fontSize = "1.7rem";
+   }
+    if(PreviousElement.length >= 17 && PreviousElement.length<23)
+    { 
+        document.querySelector('.js-Number-Display').style.fontSize = "1.3rem";
+    }
+    if(PreviousElement.length >= 23)
+    { 
+        document.querySelector('.js-Number-Display').style.fontSize = "1rem";
+    }
     PreviousCal = eval(PreviousCal);
     document.querySelector('.js-Answer-Display')
         .innerText = PreviousCal;
@@ -29,3 +67,4 @@ function DeletelastElement() {
     document.querySelector('.js-Number-Display')
         .innerText = Elem;
 }
+
